a3c8f9e6efb081d2debb084de20fc1d8cdf120a8
m-hodges <michaelcshodges@gmail.com>
2021-08-14 21:41:33 +1200

    fix up config variables, initialize tests

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = env('SPA_URL') . '/home';
+    public const HOME = '/home';
 
     /**
      * The controller namespace for the application.
